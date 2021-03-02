2035cffe49a52433999713491c37552631cddb3f
Kirill <kirillspishukk@mail.ru>
2021-03-02 20:02:00 +0300

    v0.2

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-let money = prompt("Ваш бюджет на месяц?", "");
+let money = +prompt("Ваш бюджет на месяц?", "");
 let time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
@@ -10,12 +10,54 @@ let appData = {
     savings: false 
 };
 
-let q1 = prompt("Введите обязательную статью расходов в этом месяце"),
-    q2 = prompt("Во сколько обойдется?"),
-    q3 = prompt("Введите обязательную статью расходов в этом месяце"),
-    q4 = prompt("Во сколько обойдется?");
+// let i = 0;
+// while (i < 2) {
+//     let q1 = prompt("Введите обязательную статью расходов в этом месяце"),
+//         q2 = +prompt("Во сколько обойдется?");
+    
+//     if (typeof(q1) === "string" && q1 && q2) {
+//         appData.expenses[q1] = q2;    
+//     } else {
+//        if (i >= 0) i--;
+//     }
+//     i++;  
+// }
+  
+// let i = 0;
 
-appData.expenses.q1 = q2; 
-appData.expenses.q3 = q4;
+// do {
+//     let q1 = prompt("Введите обязательную статью расходов в этом месяце"),
+//         q2 = +prompt("Во сколько обойдется?");
+    
+//     if (typeof(q1) === "string" && q1 && q2) {
+//         appData.expenses[q1] = q2;    
+//     } else {
+//         if (i >= 0) i--;
+//     }  
+//     i++;
+// } while (i < 2);
 
-alert(appData.budget / 30);
+for (let i = 0; i < 2; i++) {
+     let q1 = prompt("Введите обязательную статью расходов в этом месяце"),
+         q2 = +prompt("Во сколько обойдется?");
+    
+    if (typeof(q1) === "string" && q1 && q2) {
+        appData.expenses[q1] = q2;    
+    } else {
+       if (i >= 0) i--;
+    }     
+}
+
+appData.moneyPerDay = appData.budget / 30;
+
+if (appData.moneyPerDay < 100) {
+    console.log("low");
+} else if (appData.moneyPerDay >= 100 && appData.moneyPerDay < 2000) {
+    console.log("medium");
+} else if (appData.moneyPerDay >= 2000) {
+    console.log("high");
+} else {
+    console.log("error");
+}
+alert("Ежедневный бюджет" + appData.moneyPerDay);
+console.log(appData);
